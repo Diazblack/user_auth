5c62f162eec0212577dbf4c976bd55d7ba566d95
Cesar Jolibois <diazcd85@gmail.com>
2018-10-17 15:09:49 -0600

    added validations to User, and finish feature test

--- spec/features/users_go_to_index_and_create_a_new_account_spec.rb
+++ spec/features/users_go_to_index_and_create_a_new_account_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 describe 'User goes to index and create a new account' do
-  it "user create an account"
+  it "user create an account" do
     visit users_path
 
-    click_on(".Create A New Account")
+    click_on ".Create A New Account"
 
     expect(current_path).to eq(new_user)
 
     user_name = "Maite Delgado"
     user_email = "selo_delgado@elchiguirebipolar.com"
-    user_password
+    user_password = "1q2w3e4r"
 
     within("new_user") do
-      fill_in
+      fill_in 'user[name]', with: user_name
+      fill_in 'user[email]', with: user_email
+      fill_in 'user[password]', with: user_password
+      click_on 'Create User'
+    end
 
+    user = User.last
+    expect(user.name).to have_content(user.name)
+    expect(user.email).to have_content(user_email)
 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,9 @@ describe User, type: :model do
   describe 'Validations'  do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  
 end
